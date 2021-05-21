e9e29800e7e26ae05b113856b2d985242bb0f3f1
Kasia <kasia.bondarava@gmail.com>
2021-05-21 16:43:23 -0400

    Alert message, fetch repos

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // Init GitHub
 const github = new Github;
 
+// Init UI
+const ui = new UI;
+
 // Search input
 const searchUser = document.getElementById("searchUser");
 
@@ -16,13 +19,21 @@ searchUser.addEventListener("keyup", (e) => {
             .then(data => {
                 if(data.profile.message === "Not Found") {
                     // show alert
+                    ui.showAlert("User not found", "alert alert-danger");
+                    
+                    // clear profile
+                    ui.clearProfile();
                     
                 } else{
+                    // clear alert
+                    ui.clearAlert();
                     // show profile
-                    console.log(data);
+                    ui.showProfile(data.profile);
+                    ui.showRepos(data.repos);
                 }     
             })
     } else {
         // clear profile
+        ui.clearProfile();
     }
 });
